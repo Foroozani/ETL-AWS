aa768f0ca7a2703be0910be436b30a23cf3ae4de
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2021-05-27 16:39:22 +0200

    Add files via upload

---
+++ automatic_s3.py
@@ -0,0 +1,20 @@
+import boto3
+from secret_key import access_key_DG, secret_access_key_DG
+import os
+
+## create bucket
+
+"""resource(self, service_name, region_name=None, api_version=None,
+         use_ssl=True, verify=None, endpoint_url=None,
+         aws_access_key_id=None, aws_secret_access_key=None,
+         aws_session_token=None, config=None)"""
+aws_resource = boto3.resource('s3',aws_access_key_id = access_key_DG,
+                              aws_secret_access_key = secret_access_key_DG)
+bucket = aws_resource.Bucket('foroozani-demoboto3')
+response = bucket.create(
+    ACL='private',
+#    Bucket='string',
+    CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'}
+)
+
+print(response)
