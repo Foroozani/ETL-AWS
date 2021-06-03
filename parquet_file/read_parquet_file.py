4a75555f179e138592ffd249427793072e810bb4
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2021-06-03 14:38:59 +0200

    Update read_parquet_file.py

--- parquet_file/read_parquet_file.py
+++ parquet_file/read_parquet_file.py
@@ -3,6 +3,6 @@
 # pip install pandas, pip install pyarrow
 import pandas as pd
 
-parquet_file = r'/Users/najmeh.foroozani/PycharmProjects/pythonProject/AWS-demo/S3/userdata1.parquet'
+parquet_file = r'/Users/najmeh/PycharmProjects/userdata1.parquet'
 df = pd.read_parquet(parquet_file,engine="auto")
 df.sort_values('first_name') 
