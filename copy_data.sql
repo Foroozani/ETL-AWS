66bcf3ab63e8452a4c0b56c3b614f7f753f3b7ca
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2021-06-11 12:42:18 +0200

    Update copy_data.sql

--- copy_data.sql
+++ copy_data.sql
@@ -1,3 +1,4 @@
+-- copy data from s3 to redshift via SQL-tool
 copy users from 's3://foroozaniboto3demo/tickit/allusers_pipe.txt' 
 iam_role 'arn:aws:iam::2945541:role/MyEuRedshiftRole' 
 delimiter '|';
